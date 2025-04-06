a43c2867fc37f277e4b57ae83e821fcd07142277
MoranMishan <mishanmo@gmail.com>
2025-04-06 22:09:43 +0300

    Add src/entities/player.js

--- src/entities/player.js
+++ src/entities/player.js
@@ -1,46 +1,29 @@
 export default class Player {
-  constructor(context, x, y) {
-    this.context = context;
-    this.x = x;
-    this.y = y;
-    this.width = 50;
-    this.height = 50;
-    this.color = 'blue';
-    this.speed = 5;
-    this.jumpHeight = 100;
-    this.isJumping = false;
-    this.verticalVelocity = 0;
-  }
-
-  draw() {
-    this.context.fillStyle = this.color;
-    this.context.fillRect(this.x, this.y, this.width, this.height);
-  }
-
-  update() {
-    if (this.isJumping) {
-      this.y -= this.verticalVelocity;
-      this.verticalVelocity -= 2;
-      if (this.y >= 250) { // Ground level
-        this.y = 250;
-        this.isJumping = false;
-        this.verticalVelocity = 0;
-      }
-    }
+  constructor() {
+    this.position = { x: 0, y: 0 };
+    this.velocity = { x: 0, y: 0 };
   }
 
   moveLeft() {
-    this.x -= this.speed;
+    this.velocity.x = -5;
   }
 
   moveRight() {
-    this.x += this.speed;
+    this.velocity.x = 5;
   }
 
   jump() {
-    if (!this.isJumping) {
-      this.isJumping = true;
-      this.verticalVelocity = this.jumpHeight;
-    }
+    this.velocity.y = -10;
+  }
+
+  update() {
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    this.velocity.y += 0.5; // Gravity effect
+  }
+
+  render(context) {
+    context.fillStyle = 'blue';
+    context.fillRect(this.position.x, this.position.y, 50, 50);
   }
 }
